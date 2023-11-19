416732e134e7397f9e642df8547799bd62dbb75d
Giovanni Dylan Menegon <64831113+Epestia@users.noreply.github.com>
2023-11-19 04:51:55 +0100

    labo 6

---
+++ Jquery/Labo 6 -Jquery/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    var images = ["img/image1.jpeg", "img/image2.jpeg", "img/image3.png"];
+    var index = 0;
+
+    function changerImageDiapo() {
+        var image = $("#imageDiapo");
+        image.attr("src", images[index]);
+
+        index++;
+        if (index === images.length) {
+            index = 0;
+        }
+    }
+
+    // Change l'image toutes les 3 secondes (3000 millisecondes)
+    setInterval(changerImageDiapo, 3000);
+});
