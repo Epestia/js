cf7573b8ea8407c4481c7af9101ad5ce5cdb6892
Giovanni Dylan Menegon <64831113+Epestia@users.noreply.github.com>
2023-12-01 16:24:40 +0100

    ajax jquery

--- Jquery/Labo 8 -Jquery/js/script.js
+++ Jquery/Labo 8 -Jquery/js/script.js
@@ -1,11 +1,11 @@
-function loadPage(url, targetId) {
+function loadPage(url2, targetId) {
     $.ajax({
-        url: "./txt/textEN.txt",
+        url: url2,
         type: "GET",
         success: function(response, textStatus, xhr) {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 console.log(this);
-                $("#" + targetId).append("Texte Anglais : " + response + "<br>");
+                $("#" + targetId).append("Texte  : " + response + "<br>");
             } else {
                 alert("Erreur : " + xhr.status);
             }
@@ -15,19 +15,4 @@ function loadPage(url, targetId) {
         }
     });
 
-    $.ajax({
-        url: "./txt/textFR.txt",
-        type: "GET",
-        success: function(response, textStatus, xhr) {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                console.log(this);
-                $("#" + targetId).append("Texte Français : " + response);
-            } else {
-                alert("Erreur : " + xhr.status);
-            }
-        },
-        error: function() {
-            alert("Erreur 404 :./");
-        }
-    });
 }
